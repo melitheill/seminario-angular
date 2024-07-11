c56c942f21a04ec986dd64188978e2522061fb09
Melina Theill <meli.theill@gmail.com>
2024-07-11 15:31:39 -0300

    routing

--- apple/src/app/app-routing.module.ts
+++ apple/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppleStoreComponent } from './apple-store/apple-store.component';
+import { AppleAboutComponent } from './apple-about/apple-about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path :'', redirectTo : 'iphones', pathMatch : 'full'
+  },
+  {
+    path: 'iphones', component: AppleStoreComponent
+  },
+  {
+    path: 'about', component: AppleAboutComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
